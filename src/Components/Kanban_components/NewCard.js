0cc2590b47edd3fbed30e8d85e7f13f1dd076225
José Manuel Migoya Bussolotti <jmanuelmigoya@gmail.com>
2021-09-28 20:25:24 +0200

    started add card component

---
+++ src/Components/Kanban_components/NewCard.js
@@ -0,0 +1,43 @@
+// import taskService from ''
+
+const grid = 8;
+
+const getCardStyle = () => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+  });
+
+const submitNewCard = (event) => {
+  console.log('submitNewCard form init');
+
+  event.preventDefault()
+    const taskObject = {
+      content: newTask,
+      date: new Date(),
+      priority: Math.random() < 0.5,
+    }
+
+    taskService
+      .create(taskObject)
+      .then(response => {
+        console.log('task saved to local server');
+        setTasks(tasks.concat(response.data))
+        setNewTask('')
+      })
+}
+
+  const NewCard = () => {
+      console.log('new card component init');
+
+      return (
+        <div style={getCardStyle()}>
+          <form onSubmit={submitNewCard}>
+
+          </form>
+        </div>
+      )
+  }
+
+  export default NewCard
