7c655eb3c510160b4fdfcf662dee8fc60abd8405
migmanu <jmanuelmigoya@gmail.com>
2022-07-13 17:34:46 +0200

    change starting card index when reassigning position to 1

--- src/Components/Kanban_components/KanbanContext.js
+++ src/Components/Kanban_components/KanbanContext.js
@@ -13,125 +13,125 @@ import './kanban_styles.css'
 
 //function to move cards between columns
 const move = (source, destination, droppableSource, droppableDestination) => {
-    const sourceClone = Array.from(source);
-    const destClone = Array.from(destination);
-    let [removed] = sourceClone.splice(droppableSource.index, 1);
-    removed.column = parseInt(droppableDestination.droppableId) //update card's column field
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  let [removed] = sourceClone.splice(droppableSource.index, 1);
+  removed.column = parseInt(droppableDestination.droppableId) //update card's column field
 
-    destClone.splice(droppableDestination.index, 0, removed); //move card to new column
+  destClone.splice(droppableDestination.index, 0, removed); //move card to new column
 
-    const result = {};
-    result[droppableSource.droppableId] = sourceClone;
-    result[droppableDestination.droppableId] = destClone;
-    
-    taskService
-        .update(removed)
-        .then(
-            console.log('edit called')
-        )
+  const result = {};
+  result[droppableSource.droppableId] = sourceClone;
+  result[droppableDestination.droppableId] = destClone;
 
-    return result; 
+  taskService
+    .update(removed)
+    .then(
+      console.log('edit called')
+    )
+
+  return result;
 };
 
 //function to reorder cards moved inside same column
 const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
 
-    return result;
+  return result;
 };
 
 //update indexes in server after card movement between or into columns
 const updateIndex = (newTasks, columnNumber) => {
-    let index = 0
+  let index = 1
 
-    newTasks[columnNumber].forEach(task => {
-        if (index < newTasks[columnNumber].length) {
-            const newTask = {
-                id: task.id,
-                content: task.content,
-                date: task.date,
-                column: columnNumber,
-                position: index
-            }
-            taskService
-                .update(newTask)
-                .then(
-                    console.log(`task ${newTask.content} updated with position ${newTask.position} `)
-                )
-            index += 1
-        }
-    })
+  newTasks[columnNumber].forEach(task => {
+    if (index < newTasks[columnNumber].length) {
+      const newTask = {
+        id: task.id,
+        content: task.content,
+        date: task.date,
+        column: columnNumber,
+        position: index
+      }
+      taskService
+        .update(newTask)
+        .then(
+          console.log(`task ${newTask.content} updated with position ${newTask.position} `)
+        )
+      index += 1
+    }
+  })
 }
 
 
 const KanbanContext = () => {
-    const [tasks, setTasks] = useState([[], [], []])
+  const [tasks, setTasks] = useState([[], [], []])
 
-    useEffect(() => {
-        taskService
-          .getAll()
-          .then(response => {
-              console.log(response);
-            const toDo = response.data.filter(task => task.column === 0)
-            toDo.sort((a, b) => a.position-b.position)
+  useEffect(() => {
+    taskService
+      .getAll()
+      .then(response => {
+        console.log(response);
+        const toDo = response.data.filter(task => task.column === 0)
+        toDo.sort((a, b) => a.position - b.position)
 
-            const doing = response.data.filter(task => task.column === 1)
-            doing.sort((a, b) => a.position-b.position)
+        const doing = response.data.filter(task => task.column === 1)
+        doing.sort((a, b) => a.position - b.position)
 
-            const done = response.data.filter(task => task.column === 2)
-            done.sort((a, b) => a.position-b.position)
-              setTasks(
-                [
-                    toDo,
-                    doing,
-                    done
-                ]
-            )
-          })
-      }, [])
+        const done = response.data.filter(task => task.column === 2)
+        done.sort((a, b) => a.position - b.position)
+        setTasks(
+          [
+            toDo,
+            doing,
+            done
+          ]
+        )
+      })
+  }, [])
 
 
-    //function to save list order after movement. Adding movement between lists
-    const handleOnDragEnd = (result) => {
-        const { source, destination } = result;
+  //function to save list order after movement. Adding movement between lists
+  const handleOnDragEnd = (result) => {
+    const { source, destination } = result;
 
-        if (!destination) return;
+    if (!destination) return;
 
-        const sInd = +source.droppableId;
-        const dInd = +destination.droppableId;
+    const sInd = +source.droppableId;
+    const dInd = +destination.droppableId;
 
-        if (sInd === dInd) { //card moved inside same column
-            const items = reorder(tasks[sInd], source.index, destination.index);
-            const newTasks = [...tasks];
-            newTasks[sInd] = items;
-            updateIndex(newTasks, sInd)
-            setTasks(newTasks);
-          } else { //card moved to another column
-            const result = move(tasks[sInd], tasks[dInd], source, destination);
-            const newTasks = [...tasks];
-            newTasks[sInd] = result[sInd];
-            newTasks[dInd] = result[dInd];
+    if (sInd === dInd) { //card moved inside same column
+      const items = reorder(tasks[sInd], source.index, destination.index);
+      const newTasks = [...tasks];
+      newTasks[sInd] = items;
+      updateIndex(newTasks, sInd)
+      setTasks(newTasks);
+    } else { //card moved to another column
+      const result = move(tasks[sInd], tasks[dInd], source, destination);
+      const newTasks = [...tasks];
+      newTasks[sInd] = result[sInd];
+      newTasks[dInd] = result[dInd];
 
-            updateIndex(newTasks, sInd)
-            updateIndex(newTasks, dInd)
+      updateIndex(newTasks, sInd)
+      updateIndex(newTasks, dInd)
 
-            setTasks(newTasks)
+      setTasks(newTasks)
 
-            
-          }
+
     }
+  }
 
-    return (
-        <div className="container">
-            <DragDropContext onDragEnd={handleOnDragEnd}>
-                <Column droppableId="0" tasks={tasks[0]} />
-                <Column droppableId="1" tasks={tasks[1]} />
-                <Column droppableId="2" tasks={tasks[2]} />
-            </DragDropContext>
-        </div>
-    )
+  return (
+    <div className="container">
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Column droppableId="0" tasks={tasks[0]} setTasks={setTasks} />
+        <Column droppableId="1" tasks={tasks[1]} setTasks={setTasks} />
+        <Column droppableId="2" tasks={tasks[2]} setTasks={setTasks} />
+      </DragDropContext>
+    </div>
+  )
 }
 
 export default KanbanContext
