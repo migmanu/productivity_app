5a6a5d94c452e72aad36ede51e5f651eed99db49
migmanu <jmanuelmigoya@gmail.com>
2022-07-14 19:48:58 +0200

    try to get info on result of post method. FAILED

--- src/Services/tasks.js
+++ src/Services/tasks.js
@@ -2,23 +2,24 @@ import axios from 'axios'
 const baseUrl = '/api/tasks'
 
 const getAll = () => {
-    return axios.get(baseUrl)
+  return axios.get(baseUrl)
 }
 
 const create = newObject => {
-    return axios.post(baseUrl, newObject)
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
 }
 
 const update = (task) => {
-    const request = axios.put(`${baseUrl}/${task.id}`, task)
-    return request.then(response => response.data)
+  const request = axios.put(`${baseUrl}/${task.id}`, task)
+  return request.then(response => response.data)
 }
 
 const taskService = {
-    getAll,
-    create,
-    update
+  getAll,
+  create,
+  update
 }
 
 
-export default taskService 
+export default taskService
