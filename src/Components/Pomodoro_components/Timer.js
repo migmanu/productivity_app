0a5c34cfa421d03ac996cac293bcfc700e623917
migmanu <jmanuelmigoya@gmail.com>
2022-07-18 15:32:49 +0200

    correct description

--- src/Components/Pomodoro_components/Timer.js
+++ src/Components/Pomodoro_components/Timer.js
@@ -3,7 +3,6 @@ import styles from './timer_styles.module.css'
 
 /* 
 Simple timer component used following this tutorial: 
-https://dev.to/emmaadesile/build-a-timer-using-react-hooks-3he2
 
 Uses serInterval every second to countdown from initial time set in minutes and seconds states.
 When counter reaches zero, adds 1 to pomodoro cycle counter. 
@@ -12,60 +11,60 @@ Displays time counter and buttons.
 */
 
 const Timer = (props) => {
-    const { minutes, setMinutes, seconds, setSeconds, isActive, setIsActive, 
-        counter, setCounter } = props
-    useEffect(() => {
-        let pomodoroCycle
+  const { minutes, setMinutes, seconds, setSeconds, isActive, setIsActive,
+    counter, setCounter } = props
+  useEffect(() => {
+    let pomodoroCycle
 
-        if (isActive === true && counter > 0) {
-            pomodoroCycle = setInterval(() => {
-                let secondCounter = counter % 60 - 1;
-                let minuteCounter = Math.floor(counter / 60);
-                console.log('secondCounter is: ', secondCounter);
-                console.log('counter is: ', counter);
+    if (isActive === true && counter > 0) {
+      pomodoroCycle = setInterval(() => {
+        let secondCounter = counter % 60 - 1;
+        let minuteCounter = Math.floor(counter / 60);
+        console.log('secondCounter is: ', secondCounter);
+        console.log('counter is: ', counter);
 
-                /* 
-                The following if statement is needed in order to prevent the first 
-                second of the Timer to show '-1' within still the same minute and 
-                then jump to '58' in the following minute
-                */
-                let computedSecond = 0
-                if (String(secondCounter).length === 1) {
-                    computedSecond = `0${secondCounter}`
-                } else if (secondCounter === -1) {
-                    computedSecond = 59
-                    minuteCounter = Math.floor((counter - 1) / 60)
-                } else {
-                    computedSecond = secondCounter
-                }
-
-                const computedMinute = String(minuteCounter).length === 1 ? `0${minuteCounter}` : minuteCounter;
-
-                setSeconds(computedSecond)
-                setMinutes(computedMinute) 
-
-                setCounter(counter - 1)
-            }, 1000)
-        } else if (counter === 0) {
-            console.log(`counter is zero, isActive set to false`);
-            setIsActive(false)
-            
+        /* 
+        The following if statement is needed in order to prevent the first 
+        second of the Timer to show '-1' within still the same minute and 
+        then jump to '58' in the following minute
+        */
+        let computedSecond = 0
+        if (String(secondCounter).length === 1) {
+          computedSecond = `0${secondCounter}`
+        } else if (secondCounter === -1) {
+          computedSecond = 59
+          minuteCounter = Math.floor((counter - 1) / 60)
+        } else {
+          computedSecond = secondCounter
         }
-        
-        
 
-        return () => clearInterval(pomodoroCycle)
-    }, [isActive, counter])
+        const computedMinute = String(minuteCounter).length === 1 ? `0${minuteCounter}` : minuteCounter;
 
-    return (
-        
-        <div className={styles.time}>
-            <span className={styles.minute}>{minutes}</span>
-            <span>:</span>
-            <span className={styles.second}>{seconds}</span>
-        </div>
-        
-    )
+        setSeconds(computedSecond)
+        setMinutes(computedMinute)
+
+        setCounter(counter - 1)
+      }, 1000)
+    } else if (counter === 0) {
+      console.log(`counter is zero, isActive set to false`);
+      setIsActive(false)
+
+    }
+
+
+
+    return () => clearInterval(pomodoroCycle)
+  }, [isActive, counter])
+
+  return (
+
+    <div className={styles.time}>
+      <span className={styles.minute}>{minutes}</span>
+      <span>:</span>
+      <span className={styles.second}>{seconds}</span>
+    </div>
+
+  )
 }
 
 export default Timer
